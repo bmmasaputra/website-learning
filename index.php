1841bfce08aa6039b3abb19dbc57ba778a641579
bmmasaputra <bimagung2203@gmail.com>
2025-03-09 21:53:36 +0700

    edit index on root directory to link to login page

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php
-// Jika pengguna sudah login, arahkan ke beranda
-if (isset($_SESSION['email'])) {
-    header("Location: halPengguna/halPengguna/index.php");
-    exit();
-} else {
-    // Jika belum login, arahkan ke halaman login
-    header("Location: halPengguna/halPengguna/login.html");
-    exit();
-}
+header("Location: halPengguna/halPengguna/login.html");
 ?>
